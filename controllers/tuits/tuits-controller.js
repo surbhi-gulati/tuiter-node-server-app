4ec425c39dc603aed746e95ee86cb6f6dc392485
Surbhi Gulati <53664026+surbhi-gulati@users.noreply.github.com>
2023-04-03 13:32:26 -0400

    Update tuits-controller.js

--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -10,6 +10,16 @@ const createTuit = (req, res) => {
     res.json(newTuit);
 }
 
+const createOwnTuit = (req, res) => {
+    const newTuit = req.body;
+    newTuit._id = (new Date()).getTime()+'';
+    newTuit.likes = 0;
+    newTuit.liked = false;
+    newTuit.username = "NASA";
+    tuits.push(newTuit);
+    res.json(newTuit);
+}
+
 const findTuits = (req, res) => res.json(tuits);
 
 const updateTuit = (req, res) => {
@@ -28,6 +38,7 @@ const deleteTuit = (req, res) => {
 
 export default (app) => {
     app.post('/api/tuits', createTuit);
+    app.post('/api/tuits', createOwnTuit);
     app.get('/api/tuits', findTuits);
     app.put('/api/tuits/:tid', updateTuit);
     app.delete('/api/tuits/:tid', deleteTuit);
