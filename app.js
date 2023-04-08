6488dddeb4fb21691a4863066492d1cdb016d31d
Surbhi Gulati <surbhigulati2000@gmail.com>
2023-04-08 16:07:57 -0400

    fix typo

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ import UserController from "./controllers/users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
 import mongoose from "mongoose";
 
-mongoose.connect('mongodb://127.0.0.1:27017/tuiter');
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/tuiter'
+mongoose.connect(CONNECTION_STRING);
+
 const app = express();
 app.use(cors());
 app.use(express.json());
